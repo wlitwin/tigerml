24913778e295087a880c088b05ca9429ecc24462
Walter Litwinczyk <wlitwinczyk@mozilla.com>
2014-09-15 22:16:03 -0700

    Add missing module interface files

---
+++ src/env.mli
@@ -0,0 +1,21 @@
+module type T = 
+    functor (F : Frame.Frame) (T : (module type of Translate.Make (F))) ->
+sig
+    type access = enventry
+    and ty = Types.ty
+    and enventry = VarEntry of T.access * ty
+                 | FunEntry of T.level * (* Level *)
+                               Temp.label * (* Label *)
+                               ty list * (* Formals *)
+                               ty (* Result *)
+
+    type tenv = ty Symbol.table
+    type venv = enventry Symbol.table
+
+    val base_venv  : unit -> venv
+    val base_tenv  : unit -> tenv
+    val print_venv : venv -> unit
+    val print_tenv : tenv -> unit
+end
+
+module Make : T
---
+++ src/typecheck.mli
@@ -0,0 +1,27 @@
+module type T = 
+    functor (F : Frame.Frame) 
+            (T : (module type of Translate.Make (F)))
+            (E : (module type of Env.Make (F) (T))) ->
+sig
+
+type venv = E.enventry Symbol.table
+type tenv = Types.ty Symbol.table
+
+exception Error
+exception Impossible
+
+type expty = T.exp * Types.ty
+
+type pos = Absyn.pos
+
+val transProg : pos Absyn.exp -> expty
+
+val transVar : venv * tenv * T.level * Temp.label option * pos Absyn.var -> expty
+val transExp : venv * tenv * T.level * Temp.label option * pos Absyn.exp -> expty
+val transDec : venv * tenv * T.level * Temp.label option * pos Absyn.dec -> 
+               (venv * tenv * T.exp list)
+val transTy  :         tenv * pos Absyn.ty  -> Types.ty
+
+end
+
+module Make : T
